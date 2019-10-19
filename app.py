44dd5232fc7292ed024e1fed5edba165d1afb453
Ayan Mondal <csayan.py@gmail.com>
2019-10-19 19:03:13 +0530

    changing login system and chat room access permisons

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, json, request,url_for,redirect
+from flask import Flask, render_template, json, request,url_for,redirect,flash
+from flask_login import LoginManager,login_user,current_user,login_required,logout_user
 from wtform import *
 from models import *
 
@@ -9,7 +10,12 @@ app.config['SECRET_KEY'] = 'any secret string'
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://kmckkawkyhqpbl:adfd02bf7120f1368b1d52a02e5a5acbd75de157627c0557e1103ed070cb53a1@ec2-54-225-76-136.compute-1.amazonaws.com:5432/d8ev1f4qtpbka5'
 db = SQLAlchemy(app)
 
+login=LoginManager(app)
+login.init_app(app)
 
+@login.user_loader
+def load_user(id):
+    User.query.get(id)
 
 
 
@@ -30,6 +36,7 @@ def singup():
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
+        flash('Registered successfully. Please login.', 'success')
         return redirect(url_for('login'))
 
 
@@ -40,9 +47,28 @@ def login():
     login_form=LoginForm()
 
     if login_form.validate_on_submit():
-        return "Logged in, successfully"
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return render_template('chat.html')
 
     return render_template('login.html',form=login_form)
+
+
+@app.route('/chatroom',methods=['GET','POST'])
+def chatroom():
+    if not current_user.is_authenticated:
+        flash('Please login', 'danger')
+        return redirect(url_for('login'))
+
+    return render_template("chat.html")
+    
+    
+
+@app.route("/logout",methods=['GET'])
+def logout():
+
+    logout_user()
+    return "Logged out"
 
 if __name__ == "__main__":
     app.run(debug=False)
